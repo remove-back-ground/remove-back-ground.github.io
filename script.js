75319fef7108aa28c3d1b75368b1f67a2d507506
remove-back-ground <tomstark041@gmail.com>
2026-03-12 00:27:45 +0000

    Update script.js

--- script.js
+++ script.js
@@ -483,14 +483,14 @@ function payBinance() {
   const url = selectedPlan === 'pro'
     ? "https://s.binance.com/yVwuf5uT"
     : "https://s.binance.com/uA7xJblU";
-  startPayment(url);
+  window.open(url, '_blank');
 }
 
 function payNexa() {
   const url = selectedPlan === 'pro'
     ? "https://nexapay.one/checkout/order_264b5b1168862f013b3a98ac4b9ee7bd?sig=plsig_7c7d4c7c2652638bf96f17a0c991196277bdb10e1a45a003ec5e52e48ed6b24c"
     : "https://nexapay.one/checkout/order_97e23449f4632a11d858866e4618709c?sig=plsig_2135dcefcd4a1beb7b0fd9cf94f92023194de07ea0079dd9fa07c32856b837b4";
-  startPayment(url);
+  window.open(url, '_blank');
 }
 
 function payLemon() {
